1509e533b8e75b17cae0861ea96748ec94affbf6
Omar Hassan <85760313+omarhassan1406@users.noreply.github.com>
2021-07-05 02:09:43 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-#script to find the company name and jop title and the experince in wuzzuf for data science jops for the first 20 pages
+#script to find the company name and job title and the experience in wuzzuf for data science jobs in the first 20 pages
 
 
 from bs4 import BeautifulSoup
